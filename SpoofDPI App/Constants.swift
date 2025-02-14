77c9cc4326bb07415c2c1cbae3c0d9bffe8cdf07
Artem Solovev <artasolovev@gmail.com>
2025-02-14 16:48:02 +0000

    migrating to ByeDPI

--- SpoofDPI App/Constants.swift
+++ SpoofDPI App/Constants.swift
@@ -9,12 +9,12 @@ enum Constants {
     static let defaultLanguage = Locale.SupportedLanguage.english
     static let updatesCheckingFrequency: TimeInterval = 259200 // 3 days
     
-    static let repositoryURL = URL(string: "https://github.com/SpoofDPIApp/SpoofDPI-App")!
-    static let actualBuildNumberURL = URL(string: "https://raw.githubusercontent.com/SpoofDPIApp/SpoofDPI-App/main/Other/ActualBuildNumber.txt")!
+    static let repositoryURL = URL(string: "https://github.com/ollesss/byedpiDPI-MacOSApp")!
+    static let actualBuildNumberURL = URL(string: "https://raw.githubusercontent.com/ollesss/byedpiDPI-MacOSApp/refs/heads/main/Other/ActualBuildNumber.txt")!
     
-    static let supportEmailAddress = "SpoofDPIApp@proton.me"
+    static let supportEmailAddress = "byedpiDPIApp@proton.me"
     static let supportEmailURL = URL(string: "mailto:" + supportEmailAddress)!
     
-    static let libraryProcessNamePrefix = "spoofdpi-"
-    static let libraryVersion = "0.12.0"
+    static let libraryProcessNamePrefix = "ciadpi_"
+    static let libraryVersion = "0.16.5"
 }
